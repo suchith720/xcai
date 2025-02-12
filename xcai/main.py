d685e01499959ddfabfc7f3d11eb5e3df872315d
suchith <suchith720@gmail.com>
2025-02-13 05:23:48 +0530

    bug fix

--- xcai/main.py
+++ xcai/main.py
@@ -52,7 +52,7 @@ def get_output(pred_idx:torch.Tensor, pred_ptr:torch.Tensor, pred_score:torch.Te
     return pred
 
 # %% ../nbs/36_main.ipynb 7
-def main_run(learn, block:Union[SXCBlock, XCBlock], parse_args, n_lbl:int):
+def main_run(learn, parse_args, n_lbl:int):
     do_inference = parse_args.do_train_inference or parse_args.do_test_inference or parse_args.save_train_inference \
             or parse_args.save_test_inference or parse_args.save_repr
     if do_inference:
@@ -60,15 +60,15 @@ def main_run(learn, block:Union[SXCBlock, XCBlock], parse_args, n_lbl:int):
         os.makedirs(pred_dir, exist_ok=True)
 
         if parse_args.save_repr:
-            trn_repr, lbl_repr = learn.get_data_and_lbl_representation(learn.block.train.dset)
-            tst_repr = learn._get_data_representation(learn.block.test.dset)
+            trn_repr, lbl_repr = learn.get_data_and_lbl_representation(learn.train_dataset)
+            tst_repr = learn._get_data_representation(learn.eval_dataset)
 
             torch.save(trn_repr, f'{pred_dir}/train_repr.pth')
             torch.save(tst_repr, f'{pred_dir}/test_repr.pth')
             torch.save(lbl_repr, f'{pred_dir}/label_repr.pth')
 
         if parse_args.do_test_inference:
-            o = learn.predict(learn.block.test.dset)
+            o = learn.predict(learn.train_dataset)
             print(o.metrics)
 
             if parse_args.save_test_inference:
@@ -79,7 +79,7 @@ def main_run(learn, block:Union[SXCBlock, XCBlock], parse_args, n_lbl:int):
                 sp.save_npz(f'{pred_dir}/test_predictions.npz', pred)
 
         if parse_args.do_train_inference:
-            o = learn.predict(learn.block.train.dset)
+            o = learn.predict(learn.eval_dataset)
             print(o.metrics)
 
             if parse_args.save_train_inference:
