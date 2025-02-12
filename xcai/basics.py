285a1adecd8709ef8b082e02f46618dfee7090b5
suchith <suchith720@gmail.com>
2025-02-13 03:00:56 +0530

    added arguement parser

--- xcai/basics.py
+++ xcai/basics.py
@@ -2,7 +2,10 @@ from .core import *
 from .block import *
 from .transform import *
 from .models import *
-from .generation.trie import *
 from .metrics import *
 from .plot import *
 from .learner import *
+from .data import *
+from .sdata import *
+from .config import parse_args
+from .generation.trie import *
