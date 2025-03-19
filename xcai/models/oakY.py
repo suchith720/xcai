857d17569f56bc7ce6e9e566a506d18e1868d717
suchith <suchith720@gmail.com>
2025-03-20 03:21:53 +0530

    mogicX meta-encoder support

--- xcai/models/oakY.py
+++ xcai/models/oakY.py
@@ -507,8 +507,8 @@ class Encoder003(DistilBertPreTrainedModel):
         meta_repr, bsz = {}, embed.size(0)
         
         for m_key, m_args in meta_kwargs.items():
-            n_meta = m_args['data2ptr'].max()
-            assert torch.all(m_args['data2ptr'] == n_meta), f'All datapoints should have same number of metadata.'
+            n_meta, valid_meta_idx = m_args['data2ptr'].max(), torch.where(m_args['data2ptr'] > 0)[0]
+            assert torch.all(m_args['data2ptr'][valid_meta_idx] == n_meta), f'All datapoints should have same number of metadata.'
             
             m_input_ids, m_attention_mask = m_args['input_ids'], m_args['attention_mask']    
             if m_input_ids.shape[0] > 0: m_embed = self.meta_encode(input_ids=m_input_ids, attention_mask=m_attention_mask)
@@ -516,8 +516,8 @@ class Encoder003(DistilBertPreTrainedModel):
             meta_repr[m_key] = m_embed
                             
             m_embed = m_embed.view(bsz, -1, self.config.dim)  
-            fused_embed = self.cross_head(embed, m_embed)
-            embed = embed + fused_embed
+            fused_embed = self.cross_head(embed[valid_meta_idx], m_embed)
+            embed[valid_meta_idx] = embed[valid_meta_idx] + fused_embed
                
         return embed, meta_repr
 
